f76fc5fec7328d0b48351697fa149b0078af8953
pedrosilvabos <pedro.silva.bos@gmail.com>
2017-11-27 21:59:23 -0100

    socios working need to make it neater and finish crud @TODO event calendar with gogle calendar for members

--- app/controllers/socios_controller.rb
+++ app/controllers/socios_controller.rb
@@ -1,11 +1,22 @@
 class SociosController < ApplicationController
 	def index
+		@socio = Socio.all
 	end
+
 	def new
 	end
+
+	def show
+		@socio = Socio.find(params[:id])
+	end
 	def create
-		@socio = Socio.new(params[:socios])
+		@socio = Socio.new(socio_params)
 		@socio.save
 		redirect_to @socio
+	end
+
+	private
+	def socio_params
+		params.require(:socios).permit(:nome, :email, :morada)
 	end
 end
