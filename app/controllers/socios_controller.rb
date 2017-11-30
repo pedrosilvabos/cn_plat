7bc9661b6ea13571ba43fd47ab5590f482ca5f1a
Pedro SIlva <pedro.silva.pdl@gmail.com>
2017-11-30 12:47:17 -0100

    Delete and edit working added back button on show, when you add a new member it shows you the final results and has a back button

--- app/controllers/socios_controller.rb
+++ app/controllers/socios_controller.rb
@@ -5,7 +5,9 @@ class SociosController < ApplicationController
 
 	def new
 	end
-
+	def edit
+		@socio = Socio.find(params[:id])
+	end
 	def show
 		@socio = Socio.find(params[:id])
 	end
@@ -14,6 +16,22 @@ class SociosController < ApplicationController
 		@socio.save
 		redirect_to @socio
 	end
+
+	def update
+		@socio = Socio.find(params[:id])
+
+		if @socio.update(socio_params)
+			redirect_to @socio
+		else
+			render 'edit'
+		end
+	end
+ def destroy
+    @socio = Socio.find(params[:id])
+    @socio.destroy
+ 
+    redirect_to @socio
+  end
 
 	private
 	def socio_params
