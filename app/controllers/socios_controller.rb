4665f0496095e7b06335ade1ed48b8a222e479ca
Pedro Silva <pedro.silva.bos@gmail.com>
2017-12-02 21:42:35 -0100

    bringing up

--- app/controllers/socios_controller.rb
+++ app/controllers/socios_controller.rb
@@ -1,15 +1,11 @@
 class SociosController < ApplicationController
-before_action :authenticate_user!, except: [:index, :show]
-
 	def index
 		@socio = Socio.all
 	end
 
 	def new
 	end
-	def edit
-		@socio = Socio.find(params[:id])
-	end
+
 	def show
 		@socio = Socio.find(params[:id])
 	end
@@ -18,22 +14,6 @@ before_action :authenticate_user!, except: [:index, :show]
 		@socio.save
 		redirect_to @socio
 	end
-
-	def update
-		@socio = Socio.find(params[:id])
-
-		if @socio.update(socio_params)
-			redirect_to @socio
-		else
-			render 'edit'
-		end
-	end
- def destroy
-    @socio = Socio.find(params[:id])
-    @socio.destroy
- 
-    redirect_to @socio
-  end
 
 	private
 	def socio_params
